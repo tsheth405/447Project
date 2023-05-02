3ba4131beefd36efa6bbe0ef8525256a61d138a3
julia-nichols <98832641+julia-nichols@users.noreply.github.com>
2023-05-02 18:40:23 -0500

    Update book_flight2.php

--- book_flight2.php
+++ book_flight2.php
@@ -1,8 +1,18 @@
+<style>
+    body {
+        font-family: Arial, Helvetica, sans-serif;
+        background: url('https://images.unsplash.com/photo-1566566220367-af8d77269124?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGFpcnBvcnR8ZW58MHx8MHx8&w=1000&q=80') no-repeat center center fixed;
+        background-size: cover;
+        position: relative;
+        min-height: 100vh;
+    }
+</style>
 <?php
+
 session_start(); // start the session
 
-echo '<button style="position: absolute; top: 10px; right: 10px;" onclick="location.href=\'home.php\'">HOME</button>';
-echo '<button style="position: absolute; top: 40px; right: 10px; class="logout" onclick="location.href=\'logout.php\'">Log out</button>';
+echo '<button style="position: absolute; top: 10px; right: 10px; font-size: 15px;" onclick="location.href=\'home.php\'">HOME</button>';
+echo '<button style="position: absolute; top: 40px; right: 10px; font-size: 15px;class="logout" onclick="location.href=\'logout.php\'">Log out</button>';
 
 
 // Database connection
@@ -50,14 +60,14 @@ if (isset($_SESSION['username']) && isset($_POST['flight_number'])) {
     $result = mysqli_query($conn, $sql);
 
     if (mysqli_num_rows($result) > 0) {
-        echo '<p style="font-size: 24px; text-align: center;">Flight '.$flight_number.' has already been booked!</p>';
+        echo '<p style="font-size: 30px; margin-top: 50px; text-align: center;">Flight '.$flight_number.' has already been booked!</p>';
     } else {
         $seat_number = rand(1, 50);
         $boarding_pass = rand(1, 50)*1234;
         $sql = "INSERT INTO `Trips` (`BoardingPass_Number`, `Flight_Number`, `Seat_Number`, `User_ID`, `username`) 
         VALUES ('$boarding_pass','$flight_number', '$seat_number', '$user_id', '$name')";
         $query = mysqli_query($conn, $sql);
-        echo '<h2 style="font-size: 30px; margin-top: 50px; text-align: center;">Flight ' . $flight_number . ' has been booked</h2>';
+        echo '<h2 style="font-size: 30px; margin-top: 50px; text-align: center;">Flight ' . $flight_number . ' is now booked</h2>';
     }
     
     // Close the database connection

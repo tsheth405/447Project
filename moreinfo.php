9eab54f8acc55d028a0f56566d3f3372e7b9f743
julia-nichols <98832641+julia-nichols@users.noreply.github.com>
2023-05-01 22:12:51 -0500

    Update moreinfo.php

--- moreinfo.php
+++ moreinfo.php
@@ -1,55 +1,79 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title>Flight Information</title>
-        <meta name="description" content="">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="">
-    </head>
-    <body>
-        <h1>Flight Information</h1>
+<head>
+	<title>Flight Information</title>
+	<style>
+		table {
+			border-collapse: collapse;
+			width: 100%;
+		}
+		th, td {
+			text-align: left;
+			padding: 8px;
+			border-bottom: 1px solid #ddd;
+		}
+		tr:hover {background-color:#f5f5f5;}
+		th {
+			background-color: #4CAF50;
+			color: white;
+		}
+	</style>
+</head>
+<body>
 
-        <h2>Flight Details</h2>
-        <ul>
-            <li>Flight Number: FL123</li>
-            <li>Departure: New York City</li>
-            <li>Destination: London</li>
-            <li>Departure Time: 9:00 AM</li>
-            <li>Arrival Time: 12:00 PM</li>
-            <li>Distance: 3,470 miles</li>
-        </ul>
+<h2>Flight Information</h2>
 
-        <h2>Pilot Information</h2>
-        <ul>
-            <li>Name: John Smith</li>
-            <li>Years of Experience: 10</li>
-            <li>Certifications: Commercial Pilot, Multi-Engine Rating, Instrument Rating</li>
-            <li>Training: Completed Boeing 737 simulator training</li>
-        </ul>
+<table>
+	<tr>
+		<th>Flight Number</th>
+		<th>Arrival Terminal</th>
+		<th>Departure Terminal</th>
+		<th>Arrival Gate</th>
+		<th>Departure Gate</th>
+		<th>Status</th>
+		<th>Pilot Name</th>
+		<th>Aircraft Type</th>
+	</tr>
 
-        <h2>Flight Attendant Information</h2>
-        <ul>
-            <li>Name: Jane Doe</li>
-            <li>Years of Experience: 5</li>
-            <li>Training: Completed FAA-mandated training in safety procedures and emergency protocols</li>
-        </ul>
+	<?php
+		// Connect to the database
+		$servername = "localhost";
+        $username = "root";
+        $password = "";
+        $dbname = "test";
+		$conn = new mysqli($servername, $username, $password, $dbname);
 
-        <h2>City Information</h2>
-        <ul>
-            <li>New York City:</li>
-            <ul>
-                <li>Population: 8,336,817</li>
-                <li>Time Zone: Eastern Time (GMT -4)</li>
-                <li>Weather: Mostly sunny, high of 72°F</li>
-            </ul>
-            <li>London:</li>
-            <ul>
-                <li>Population: 9,126,366</li>
-                <li>Time Zone: Greenwich Mean Time (GMT +1)</li>
-                <li>Weather: Partly cloudy, high of 59°F</li>
-            </ul>
-        </ul>
-    </body>
+		// Check connection
+		if ($conn->connect_error) {
+			die("Connection failed: " . $conn->connect_error);
+		}
+
+		// Retrieve the flight information from the database
+		$sql = "SELECT * FROM Flight_Info";
+		$result = $conn->query($sql);
+
+		// Display the information in a table
+		if ($result->num_rows > 0) {
+			while($row = $result->fetch_assoc()) {
+				echo "<tr>";
+				echo "<td>" . $row["Flight_Number"] . "</td>";
+				echo "<td>" . $row["Arrival_Terminal"] . "</td>";
+				echo "<td>" . $row["Departure_Terminal"] . "</td>";
+				echo "<td>" . $row["Arrival_Gate"] . "</td>";
+				echo "<td>" . $row["Departure_Gate"] . "</td>";
+				echo "<td>" . $row["Status"] . "</td>";
+				echo "<td>" . $row["Pilot_Name"] . "</td>";
+				echo "<td>" . $row["Aircraft_Type"] . "</td>";
+				echo "</tr>";
+			}
+		} else {
+			echo "0 results";
+		}
+
+		$conn->close();
+	?>
+
+</table>
+
+</body>
 </html>

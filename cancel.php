946316632459e3c6ecd2bf3654ad555d63bc8138
julia-nichols <98832641+julia-nichols@users.noreply.github.com>
2023-05-02 17:31:07 -0500

    Update cancel.php

--- cancel.php
+++ cancel.php
@@ -21,6 +21,7 @@
 
         // Home button
         echo '<button style="position: absolute; top: 10px; right: 10px;" onclick="location.href=\'home.php\'">HOME</button>';
+        echo '<button style="position: absolute; top: 40px; right: 10px; class="logout" onclick="location.href=\'logout.php\'">Log out</button>';
 
 		// Get the flight number from the query string
 		$flight_number = $_GET['flight'];
@@ -28,7 +29,7 @@
 		// Delete the flight from the trips table
 		$sql = "DELETE FROM trips WHERE Flight_Number='$flight_number'";
 		if ($conn->query($sql) === TRUE) {
-			echo "Flight $flight_number has been cancelled and removed from your trip list";
+            echo '<p style="font-size: 24px; text-align: center;">Flight '.$flight_number.' has been cancelled and removed from your trip list!</p>';
 		} else {
 			echo "Error deleting flight: " . $conn->error;
 		}

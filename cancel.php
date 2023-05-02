91ae2ca049cbe1ad0609db912790abf67057d698
julia-nichols <98832641+julia-nichols@users.noreply.github.com>
2023-05-02 18:40:42 -0500

    Update cancel.php

--- cancel.php
+++ cancel.php
@@ -1,3 +1,21 @@
+<style>
+    body {
+        font-family: Arial, Helvetica, sans-serif;
+        background: url('https://images.unsplash.com/photo-1566566220367-af8d77269124?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGFpcnBvcnR8ZW58MHx8MHx8&w=1000&q=80') no-repeat center center fixed;
+        background-size: cover;
+        position: relative;
+        min-height: 100vh;
+    }
+    p.message {
+        color: #3a3f44;
+        font-size: 40px;
+        text-align: center;
+        position: absolute;
+        top: 20%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+    }
+</style>
 <!DOCTYPE html>
 <html>
 <head>
@@ -29,7 +47,7 @@
 		// Delete the flight from the trips table
 		$sql = "DELETE FROM trips WHERE Flight_Number='$flight_number'";
 		if ($conn->query($sql) === TRUE) {
-            echo '<p style="font-size: 24px; text-align: center;">Flight '.$flight_number.' has been cancelled and removed from your trip list!</p>';
+            echo '<p class="message">Flight '.$flight_number.' has been cancelled and removed from your trip list!</p>';
 		} else {
 			echo "Error deleting flight: " . $conn->error;
 		}
